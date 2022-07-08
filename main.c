06faf72ecdbe921c92e49c889f24ca578bffc8f6
Peter Johnson <integral.sketch@gmail.com>
2022-07-08 15:06:08 -0400

    Added main help function as well as argument tester.

--- main.c
+++ main.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
+#define ANSIS "\x1b["
+#define ANSIM ";"
+#define ANSIE "m"
 
-int main() {
-	printf("microtrash: version 1.0\nusage:\n\t'trash <FILE...>': push file(s) to trash.\n\t\tflags: -r: removes file from directory\n\n\t'trash compress': Compress trash directory into tar.gz file.\n\n\t'trash expand': Expand trash file out to directory. Run automatically on default 'trash <FILE...>' command if directory is compressed.");
+
+int main(int argc, const char** argv) {
+	printf("%smicrotrash: version 1.0%s\nusage:\n\t'trash <FILE...>': push file(s) to trash.\n\t\tflags: -r: removes file from directory\n\n\t'trash compress': Compress trash directory into tar.gz file.\n\n\t'trash expand': Expand trash file out to directory. Run automatically on default 'trash <FILE...>' command if directory is compressed.\nArgs: ",
+			"\x1b[35;49m", "\x1b[39;49m");
+	
+	// Loop over argv and print them
+	for (int i = 0; i<argc; i++) {
+		printf("%s ", argv[i]);
+	};
+
+	// newline
+	printf("\n");
 	return 0;
 };
